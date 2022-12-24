2c77844a0e2cf0f815cd7506d0fd7bf3a2546f78
Zaiste <oh@zaiste.net>
2022-12-24 15:47:40 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import { promises as fs } from 'node:fs';
+import GitURLParse from 'git-url-parse';
+import { simpleGit } from 'simple-git';
+
+const git = simpleGit();
+
+export const clone = async (repo: string, dest: string, ref = 'main') => {
+  const { href: repoURL } = GitURLParse(repo);
+  await git.clone(repoURL, dest, { '--branch': ref, '--depth': 1 });
+  await fs.rm(`${dest}/.git`, { recursive: true });
+};
+
+const args = process.argv.slice(2);
+const [repo, dest] = args;
+
+await clone(repo, dest);
